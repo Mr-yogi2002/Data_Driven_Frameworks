59d614c0e4271b73eff4bd1654d78a42dc374775
Mr-yogi2002 <rtyogesh05@gmail.com>
2024-09-27 16:55:20 +0530

    Add files via upload

---
+++ DDTF---PAT-main/Test_Locators/homepage.py
@@ -0,0 +1,14 @@
+class OrangeHRM_Locators:
+    #webpage locators
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username = 'username'
+    password = 'password'
+    login_btn = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+    profile_img = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/img'
+    logout_btn = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a'
+    dashboard_url = 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'
+
+    # excel utilities
+    excel_file_path = "C:\\Users\\rtyog\\OneDrive\\Desktop\\Software_Testing_Code\\DDTF---PAT-main\\DDTF---PAT-main\\Test_Data\\test_data.xlsx"
+    sheet_no = "Sheet1"
+    
